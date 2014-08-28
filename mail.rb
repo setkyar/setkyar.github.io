39026fa2c77df4559bae25b8c5e9aa8ae0311e42
setkyar <setkyar16@gmail.com>
2014-08-29 00:18:47 +0630

    Testing

---
+++ mail.rb
@@ -0,0 +1,14 @@
+equire 'net/smtp'
+
+message = <<MESSAGE_END
+From: Private Person <me@fromdomain.com>
+To: A Test User <test@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+MESSAGE_END
+
+Net::SMTP.start('localhost') do |smtp|
+  smtp.send_message message, 'setkyar16@gmail.com', 
+                             'setkyar16@gmail.com'
+end
